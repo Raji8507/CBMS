14943910f72af3101268d725263d8fd0f1c4826d
boominathan2355 <boominathanalagirisamy@gmail.com>
2025-12-14 13:22:23 +0530

    update webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,65 +1,62 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: "./src/index.jsx",
-
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.[contenthash].js",
-    publicPath: "/CBMS/",
-    clean: true
-  },
-
-  mode: process.env.NODE_ENV || "development",
-
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|ico)$/i,
-        type: "asset/resource"
-      }
-    ]
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"   // ✅ FIX
-    }),
-    new Dotenv({
-      systemvars: true
-    })
-  ],
-
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public")
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+        publicPath: '/CBMS/'
     },
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-    proxy: [
-      {
-        context: ["/api"],
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: false
-      }
-    ]
-  }
+    mode: process.env.NODE_ENV || 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|ico)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new Dotenv({
+            systemvars: true, // load system environment variables as well
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        port: 3000,
+        server: 'https',
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+        proxy: [
+            {
+                context: ['/api'],
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                secure: false,
+            },
+        ],
+    },
 };
